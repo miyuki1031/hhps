2f5dbcadae3ccaad4a35d0e12c7e459e4b37d1aa
ｍ <62457140+miyuki1031@users.noreply.github.com>
2025-12-28 00:38:36 +0900

    どうさかくにん

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ export default async function Footer() {
     return (
         <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
             <aside className="m-auto">
-                © 2025 - {new Date().getFullYear()} {SITE_INFO.NAME} | Creating
-                with heart.
+                　　　　 © 2025 - {new Date().getFullYear()} {SITE_INFO.NAME} |
+                Creating with heart.
                 <br />
                 {SITE_INFO.MESSAGE}
             </aside>
