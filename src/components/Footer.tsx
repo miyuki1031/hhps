456fb910ecf245c26efb9b722d29f21eadcd350f
ｍ <62457140+miyuki1031@users.noreply.github.com>
2025-12-28 00:39:43 +0900

    どうさかくにん2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import { SITE_INFO } from "../lib/constants";
 
 export default async function Footer() {
+    const A;
+
     return (
         <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
             <aside className="m-auto">
-                　　　　 © 2025 - {new Date().getFullYear()} {SITE_INFO.NAME} |
-                Creating with heart.
+                © 2025 - {new Date().getFullYear()} {SITE_INFO.NAME} | Creating
+                with heart.
                 <br />
                 {SITE_INFO.MESSAGE}
             </aside>
