ab53c3eecc8b051f1cc82d1726cbc04ea2ea8f76
miyuki1031 <heavenmiyuki@hotmail.com>
2026-07-19 16:06:52 +0900

    確認画面にてWebsocket発生しているエラー対応

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    allowedDevOrigins: ['localhost:3000', '192.168.0.15:3000', '192.168.0.15'],
-  };
+  allowedDevOrigins: ['localhost:3000', '192.168.0.15:3000', '192.168.0.15', '10.0.0.2', '10.0.0.2:3000'],
+  // assetPrefix: ','
+};
 
 export default nextConfig;
